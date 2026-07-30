0cedfa567d464a480a2fc37edd8914cfe76a7fde
QuocCuong777 <cuongtran0961@gmail.com>
2026-07-30 09:18:39 +0700

    update App.js

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import DetailPage from "./pages/DetailPage";
+import AboutPage from "./pages/AboutPage";
+import Login from "./components/Login";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movie/:id" element={<DetailPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          {/* Sửa dòng bên dưới */}
+          <Route path="/login" element={<Login />} /> 
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
